da5f8fc8e1109d4fea961a13342301e9c0fd4f8e
unknown <oppongcoffie27@gmail.com>
2026-05-24 07:25:12 +0000

    widget

--- src/components/DodoWidget.tsx
+++ src/components/DodoWidget.tsx
@@ -1,3 +1,55 @@
+// // components/DodoWidget.tsx
+// import { useEffect } from "react";
+
+// declare global {
+//   interface Window {
+//     DodoWidget?: any;
+//   }
+// }
+
+// export default function DodoWidget() {
+//   useEffect(() => {
+//     const loadDodoWidget = async () => {
+//       // Avoid loading multiple times
+//       if (!document.querySelector("#dodo-css")) {
+//         const css = document.createElement("link");
+//         css.id = "dodo-css";
+//         css.rel = "stylesheet";
+//         css.href = "/dodo-widget.css";
+//         document.head.appendChild(css);
+//       }
+
+//       if (!document.querySelector("#dodo-script")) {
+//         const script = document.createElement("script");
+//         script.id = "dodo-script";
+//         script.src = "dodo-widget.iife.js";
+//         script.async = true;
+//         script.onload = () => {
+//           if (window.DodoWidget) {
+//             window.DodoWidget.init({
+//               title: "Dodo",
+//               widgetKey: "t8i2i0vjYVh4UwSqPgbcOgI3HcSH/BzKAW9Y0yPvcHo=",
+//               widgetTheme: "theme-dark-blue",
+//               AiName: "Dodo Assistant",
+//             });
+//           }
+//         };
+//         document.body.appendChild(script);
+//       } else if (window.DodoWidget) {
+//         window.DodoWidget.init({
+//           title: "Dodo",
+//           widgetKey: "t8i2i0vjYVh4UwSqPgbcOgI3HcSH/BzKAW9Y0yPvcHo=",
+//           widgetTheme: "theme-dark-blue",
+//           AiName: "Dodo Assistant",
+//         });
+//       }
+//     };
+
+//     loadDodoWidget();
+//   }, []);
+
+//   return null; // The widget renders externally
+// }
 // components/DodoWidget.tsx
 import { useEffect } from "react";
 
@@ -10,37 +62,41 @@ declare global {
 export default function DodoWidget() {
   useEffect(() => {
     const loadDodoWidget = async () => {
-      // Avoid loading multiple times
-      if (!document.querySelector("#dodo-css")) {
+      // Inject CSS if not already added
+      if (!document.querySelector("#dodo-widget-css")) {
         const css = document.createElement("link");
-        css.id = "dodo-css";
+        css.id = "dodo-widget-css";
         css.rel = "stylesheet";
         css.href = "https://widget.usedodo.com/dodo-widget.css";
         document.head.appendChild(css);
       }
 
-      if (!document.querySelector("#dodo-script")) {
+      // Inject Script if not already added
+      if (!document.querySelector("#dodo-widget-script")) {
         const script = document.createElement("script");
-        script.id = "dodo-script";
+        script.id = "dodo-widget-script";
         script.src = "https://widget.usedodo.com/dodo-widget.iife.js";
         script.async = true;
+
         script.onload = () => {
           if (window.DodoWidget) {
             window.DodoWidget.init({
-              title: "AAKT Support Bot",
-              widgetKey: "kq66XepGJaydL3U3AJTa+hzTCx7c3mlaWiDSqSc+TvQkuo+inxz91CFux4xswX4M9klLirIEw98E4/tvHB5aoQ==",
+              title: "Aakt AI",
+              widgetKey: "JLqPop8c/dQhbseMbMF+DPZJS4qyBMPfBOP7bxweWqE=",
               widgetTheme: "theme-dark-blue",
-              AiName: "AAKT Support Bot",
+              AiName: "Aakt",
             });
           }
         };
+
         document.body.appendChild(script);
       } else if (window.DodoWidget) {
+        // If script already loaded, just init
         window.DodoWidget.init({
-          title: "AAKT Support Bot",
-          widgetKey: "kq66XepGJaydL3U3AJTa+hzTCx7c3mlaWiDSqSc+TvQkuo+inxz91CFux4xswX4M9klLirIEw98E4/tvHB5aoQ==",
+          title: "Aakt Ai",
+          widgetKey: "JLqPop8c/dQhbseMbMF+DPZJS4qyBMPfBOP7bxweWqE=",
           widgetTheme: "theme-dark-blue",
-          AiName: "AAKT Support Bot",
+          AiName: "Aakt",
         });
       }
     };
@@ -48,5 +104,5 @@ export default function DodoWidget() {
     loadDodoWidget();
   }, []);
 
-  return null; // The widget renders externally
+  return null; // Widget is injected externally
 }
